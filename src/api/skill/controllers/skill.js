52c8aa6606f49fafba37657230b9143c84bcefe5
Zain Abbas <zainabbas4081@gmail.com>
2026-07-07 23:07:09 +0500

    fix skills add

--- src/api/skill/controllers/skill.js
+++ src/api/skill/controllers/skill.js
@@ -2,5 +2,23 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::skill.skill");
+module.exports = createCoreController("api::skill.skill", ({ strapi }) => ({
+  async create(ctx) {
+    const user = ctx.state.user;
 
+    if (!user) {
+      return ctx.unauthorized("You must be logged in.");
+    }
+
+    const { data } = ctx.request.body;
+
+    const skill = await strapi.documents("api::skill.skill").create({
+      data: {
+        ...data,
+        owner: user.documentId,
+      },
+    });
+
+    return this.transformResponse(skill);
+  },
+}));
