05dc7a48269a5e2a3df8ff3c8be207540e87d938
Zain Abbas <zainabbas4081@gmail.com>
2026-07-08 18:57:00 +0500

    DFDF

--- src/middlewares/request-logger.js
+++ src/middlewares/request-logger.js
@@ -3,12 +3,25 @@
 module.exports = () => {
   return async (ctx, next) => {
 
-      console.log("==================================");
-      console.log("METHOD :", ctx.method);
-      console.log("URL    :", ctx.url);
+
+      console.log("\n================ REQUEST ================");
+      console.log("Method       :", ctx.method);
+      console.log("URL          :", ctx.request.url);
+      console.log("Content-Type :", ctx.request.headers["content-type"]);
+      console.log("Authorization:", ctx.request.headers["authorization"] || "None");
+      console.log("Body:");
       console.dir(ctx.request.body, { depth: null });
-      console.log("==================================");
+      console.log("=========================================\n");
+
 
     await next();
+
+
+      console.log("\n================ RESPONSE ===============");
+      console.log("Status:", ctx.status);
+      console.log("Body:");
+      console.dir(ctx.body, { depth: null });
+      console.log("=========================================\n");
+      console.log("===================END RESQUEST============\n");
   };
 };
