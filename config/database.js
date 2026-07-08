b93949be45cd7ce718e2ffcbbbf11404b45fd6aa
Abid Hussain <engrabidhussain5@gmail.com>
2026-07-08 16:06:21 +0300

    HJHJ

--- config/database.js
+++ config/database.js
@@ -1,14 +1,37 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "community_skills_exchange"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "postgres"), 
-      ssl: false,
+const path = require("path");
+
+module.exports = ({ env }) => {
+  const client = env("DATABASE_CLIENT", "postgres");
+
+  const connections = {
+    postgres: {
+      connection: {
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "community_skills_exchange"),
+        user: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "postgres"),
+        ssl: false,
+      },
     },
-    debug: false,
-  },
-});
+
+    sqlite: {
+      connection: {
+        filename: path.join(
+          __dirname,
+          "..",
+          env("DATABASE_FILENAME", ".tmp/data.db")
+        ),
+      },
+      useNullAsDefault: true,
+    },
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      debug: false,
+    },
+  };
+};
